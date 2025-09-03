76d248d37fc8e057da2d6fbc44006aac4dcd20a8
Areesh Ali <areesh.192003@gmail.com>
2025-09-03 06:40:35 +0500

    feat: remove SignInPage, SignUpPage, ChatView, and HomePage components

--- packages/web/src/feature/auth/SignInPage.tsx
+++
@@ -1,9 +0,0 @@
-import { SignInForm } from "../../components/SignInForm";
-
-export default function SignInPage() {
-  const handleSignIn = (data: { email: string; password: string }) => {
-    // TODO: Call login API
-    console.log("Sign in:", data);
-  };
-  return <SignInForm onSubmit={handleSignIn} />;
-}
--- packages/web/src/feature/auth/SignUpPage.tsx
+++
@@ -1,9 +0,0 @@
-import { SignUpForm } from "../../components/SignUpForm";
-
-export default function SignUpPage() {
-  const handleSignUp = (data: { email: string; password: string; username: string }) => {
-    // TODO: Call register API
-    console.log("Sign up:", data);
-  };
-  return <SignUpForm onSubmit={handleSignUp} />;
-}
